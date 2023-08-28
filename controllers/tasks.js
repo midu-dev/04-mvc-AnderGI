317e8cfac66a44198f2f37fab6515781e0797272
AnderGI <agibarguen@gmail.com>
2023-08-28 13:13:56 +0200

    Exercise 4, I will add Zod validation to the description

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,20 @@
 // importar modelo
+import { TaskModel } from "../models/task.js";
 
 export const getAllTasks = (req, res) => {
-  const tasks = // ????
-  res.json(tasks)
-}
+  const tasks = TaskModel.getAllTasks();
+  res.json(tasks);
+};
 
 export const addTask = (req, res) => {
-  const task = // ????
-  res.json(task)
-}
+  const newTask = req.body;
+  const task = TaskModel.addTask(newTask);
+  res.json(task);
+};
 
 export const deleteTask = (req, res) => {
-  // ????
-  res.json({ message: 'Deleted successfully' })
-}
+  // /tasks/:id <-- params de req
+  const { id } = req.params;
+  TaskModel.deleteTask(id);
+  res.json({ message: "Deleted successfully" });
+};
